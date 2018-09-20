60c5a6893aab8898f4c9090a75f706fc1421609d
MatthewChang <mjchang07@gmail.com>
2018-09-20 12:06:51 -0400

    tests for sub record moduel

--- backend/src/SchemaMatch.hs
+++ backend/src/SchemaMatch.hs
@@ -16,21 +16,21 @@ import GHC.TypeLits
 
 type Store = Rec '["name" := String, "money" := Int]
 
-type Schema = '["user" := User, "money" := Int]
-type User = Rec '["name" := String, "age" := Int]
+--type Schema = '["user" := User, "money" := Int]
+--type User = Rec '["name" := String, "age" := Int]
 
-type GoodTestRecord = '["user" := User]
-type EasyBadRecord = '["user" := Int]
-type HardGoodRecord = '["user" := Rec '["name" := String]]
-type HardBadRecord = '["user" := Rec '["name" := Int]]
+--type GoodTestRecord = '["user" := User]
+--type EasyBadRecord = '["user" := Int]
+--type HardGoodRecord = '["user" := Rec '["name" := String]]
+--type HardBadRecord = '["user" := Rec '["name" := Int]]
 
 
 
-test :: Has "name" '["name" := String] Int => Int
-test = 5
+--test :: Has "name" '["name" := String] Int => Int
+--test = 5
 
-testBad :: HasOf '["name" := String] '["name" := Int] => Int
-testBad = 5
+--testBad :: HasOf '["name" := String] '["name" := Int] => Int
+--testBad = 5
 
 -- Returns the type from a record property if it exists, error otherwise
 type family RecTy (l :: Symbol) (lts :: [*]) :: * where
@@ -41,23 +41,14 @@ type family RecTy (l :: Symbol) (lts :: [*]) :: * where
 type family UnRec (r :: * ) :: [*] where
     UnRec (Rec a) = a
 
+-- Returns a constraint that passes when the list of properties tls has symbol l with type t
+-- if t is also a Record, we check recirsivly that t is a subrecord of lts[l] (the element of lts that has label l) 
+-- naturally, the constraint will fail if t is a Record but lts[l] is not
 type family HasR (l :: Symbol) (lts :: [*]) (t :: *) :: Constraint where
   HasR l lts (Rec a) = (SubRecordR a (UnRec (RecTy l lts)))
   HasR l lts t = Has l lts t
 
---type family SubRecTy (l :: Symbol) (lts :: [*]) :: [*] where
-    --SubRecTy l (l := (Rec t) ': lts) = t
-    --SubRecTy q (l := t ': lts) = SubRecTy q lts
-
---
---type family HasR (l :: Symbol) (lts :: [*]) (t :: *) :: Constraint where
-  --HasR l lts (Rec a) = (SubRecordR a (SubRecTy l lts))
-  --HasR l lts t = Has l lts t
-
+-- Returns a constraint that passes when the list of properties req represents a recursive sub record of tls
 type family SubRecordR (req :: [*]) (lts :: [*]) :: Constraint where
     SubRecordR (l := t ': req) lts = (HasR l lts t, SubRecordR req lts)
     SubRecordR '[] lts = 'True ~ 'True
-
-perform :: (SubRecordR HardGoodRecord Schema) => Int
-perform = 5
-
---
+++ backend/test/SubRecordTypeCheckSpec.hs
@@ -0,0 +1,56 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-} -- To test type checking
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE TypeOperators             #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedLabels #-}
+
+module SubRecordTypeCheckSpec where
+
+import Test.Hspec
+import SchemaMatch
+import Test.ShouldNotTypecheck (shouldNotTypecheck)
+import           SuperRecord
+import           SuperRecordExtra
+
+
+type Schema = '["user" := User, "money" := Int]
+type User = Record '["name" := String, "age" := Int]
+
+type GoodTestRecord = '["user" := User]
+type EasyBadRecord = '["user" := Int]
+type HardGoodRecord = '["user" := Record '["name" := String]]
+type HardBadRecord = '["user" := Record '["name" := Int]]
+
+
+single = (& rnil)
+
+perform :: (SubRecordR input Schema) => (Rec input) -> Int
+perform _ = 5
+
+int :: Int
+int = 5
+
+str :: String
+str = "test"
+
+spec :: Spec
+spec = do
+  describe "Recursive Sub Record Constraint" $ do
+    it "should disallow mismatched names" $ do
+      shouldNotTypecheck $ perform $ #nokey := int & rnil
+
+    it "should allow matched names" $ do
+      5 `shouldBe` (perform $ #money := int & rnil)
+
+    it "should disallow mismatched types" $ do
+      shouldNotTypecheck $ (perform $ #money := str & rnil)
+
+    it "should allow nested sub records" $ do
+      5 `shouldBe` (perform $ single $ #user := (single $ #name := str))
+      5 `shouldBe` (perform $ single $ #user := (#name := str &! #age := int))
+
+    it "should disallow bad nested sub records" $ do
+      shouldNotTypecheck (perform $ single $ #user := (single $ #name := int))
+      shouldNotTypecheck (perform $ single $ #user := (single $ #other := int))
